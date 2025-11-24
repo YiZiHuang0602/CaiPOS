2425725c2a251a61f1f670defbe040486993cb8d
LAPTOP-RER1Q3CK\User <kelly26652816@gmail.com>
2025-11-24 18:06:19 +0800

    no message

--- CaiPOS/Models/UserManagement.cs
+++ CaiPOS/Models/UserManagement.cs
@@ -6,6 +6,7 @@ namespace CaiPOS.Models
     {
         [Key]
         public Guid UserId {  get; set; } = Guid.NewGuid();
+        public Guid UserNumber { get; set; } = Guid.NewGuid();
 
         public string UserName { get; set; }
 
